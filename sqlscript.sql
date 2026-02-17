552c65dc062265a3ec5009191c191aaebda40c83
AtharvTungatkar <atharvpt52@gmail.com>
2026-02-17 13:04:20 -0500

    Setup and Data Loading Complete

---
+++ sqlscript.sql
@@ -0,0 +1,4 @@
+create database solar_energy_analysis;
+use solar_energy_analysis;
+# After creating the database go to the database/tables and then import the dataset.
+select * from solar_energy_worldwide;
